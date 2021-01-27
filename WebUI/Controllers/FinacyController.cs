224e5362b47fe0dbb8d8140cb5b0d3bb63c78ae2
EnzoCh <yuhengchen9@gmail.com>
2021-01-27 23:59:08 +0800

    添加项目文件。

---
+++ WebUI/Controllers/FinacyController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class FinacyController : Controller
+    {
+        // GET: Finacy
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Finacy/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Finacy/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Finacy/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Finacy/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Finacy/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Finacy/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Finacy/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
